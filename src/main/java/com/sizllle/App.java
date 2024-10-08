d3ce70fd69b7067ebe55066805b45e0b790d6132
Sizllle <vladislaw.kuzminov2017@gmail.com>
2024-10-08 17:28:44 +0300

    Create a project (Maven Archetype)

---
+++ src/main/java/com/sizllle/App.java
@@ -0,0 +1,13 @@
+package com.sizllle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
